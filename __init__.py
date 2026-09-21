c1a0c37f8ceeae06474defc5242837908d1bfbf2
kaniskarajput30-cyber <kaniskarajput30@gmail.com>
2026-09-21 16:03:55 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""Lightweight monocular RGB sparse-point-cloud SLAM (visual odometry + local map)."""
-
-from .vo import MonocularSLAM, SLAMConfig, SLAMResult
-
-__all__ = ["MonocularSLAM", "SLAMConfig", "SLAMResult"]
